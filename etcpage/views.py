683bb325da7d86560acedae9ca2c19a37f21b941
Bligh Park <sy131.park@gmail.com>
2017-04-08 16:24:53 +0900

    bootstrap root_category 수정 product 정보 가져오기

--- etcpage/views.py
+++ etcpage/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from product.models import Category, Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'etcpage/home.html')
+    return render(request, 'etcpage/home.html', context={
+        'root_categories': Category.objects.filter(parent__isnull=True).order_by('priority'),
+        'products': Product.objects.all().order_by('-score', '-create_at'[:5])
+    })
