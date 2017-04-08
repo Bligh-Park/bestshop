567a6e38ca8e9e61b82c364421c324ac0e317261
Bligh Park <sy131.park@gmail.com>
2017-04-08 16:30:55 +0900

    화면에 상품정보 표기해주기 User 에 urls 추가 - 로그인 페이지 구현할 예정

--- etcpage/views.py
+++ etcpage/views.py
@@ -6,5 +6,5 @@ from product.models import Category, Product
 def home(request):
     return render(request, 'etcpage/home.html', context={
         'root_categories': Category.objects.filter(parent__isnull=True).order_by('priority'),
-        'products': Product.objects.all().order_by('-score', '-create_at'[:5])
+        'products': Product.objects.all().order_by('-score', '-created_at')[:5]
     })
---
+++ user/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from user import views
+
+
+urlpatterns = [
+    url(r'^login$', views.login_page),
+]
