84f7d0de128ac5e9aa5e6c6f0174984bc66a711b
Bligh Park <sy131.park@gmail.com>
2017-04-08 18:59:06 +0900

    django_summernote admin 에 등록

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
+
 from product.models import Category, Product, DateRangeDiscount
 
 admin.site.register(Category)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(SummernoteModelAdmin):
+    pass
+
+
+#admin.site.register(Product, ProductAdmin)
 admin.site.register(DateRangeDiscount)
