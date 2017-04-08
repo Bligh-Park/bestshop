49e8dc6df13a6e0e48b32ef7d1f7b0e6378f176d
Bligh Park <sy131.park@gmail.com>
2017-04-08 15:27:08 +0900

    Category modeling 수정 - parent 에 blank 가능하도록   => 관리자에서 수정가능하도록 설정

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=64)
-    parent = models.ForeignKey('self', null=True)
+    parent = models.ForeignKey('self', null=True, blank=True)
     priority = models.IntegerField()
 
     class Meta:
